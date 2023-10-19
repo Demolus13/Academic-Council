01a591c33b8c9b71fdab2dba43af5b4f3bc80652
choudharyNetram <choudharynetram7@gmail.com>
2023-10-19 20:06:40 +0530

    Change folder path

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import '../styles/Home.css';
 import { Link } from "react-router-dom";
-import Teamwork from './Images/teamwork.svg'
+import Teamwork from '../Images/teamwork.svg'
 export default function Home(props) {
   return (
     <>
