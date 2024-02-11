0db4d7f044e4eadf4055a5bc3938d141c0dad181
sisodiyaanay <sisodiyaanay@iitgn.ac.in>
2024-02-11 12:23:26 +0530

    hide m. tech option for now

--- src/components/GradeTracker.js
+++ src/components/GradeTracker.js
@@ -517,7 +517,7 @@ export default function GradeTracker() {
                   <option className="opt" value="B.Tech">
                     B.Tech
                   </option>
-                  <option value="M.Tech">M.Tech</option>
+                  {/* <option value="M.Tech">M.Tech</option> */}
                   <option value="Dual Majors">Dual Majors</option>
                 </select>
 
