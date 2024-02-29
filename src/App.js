56ae45852123651ab4ad54fceab38fa59ff07cc9
Netram Choudhary <choudharynetram7@gmail.com>
2024-02-29 11:05:39 +0530

    cleaned up repo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './styles/App.css';
 import React, { useState } from 'react';
 import { Routes, Route, HashRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
